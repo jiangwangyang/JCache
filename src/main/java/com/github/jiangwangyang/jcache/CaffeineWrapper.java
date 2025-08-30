ec999e75806ee4c4a5624e9a80170ec473ef065c
蒋汪洋 <402233792@qq.com>
2025-08-30 13:41:19 +0800

    为Caffeine增加JCache包装类

---
+++ src/main/java/com/github/jiangwangyang/jcache/CaffeineWrapper.java
@@ -0,0 +1,93 @@
+package com.github.jiangwangyang.jcache;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.Expiry;
+
+import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Function;
+
+public class CaffeineWrapper<K, V> implements JCache<K, V> {
+
+    final Cache<K, ValueWrapper<V>> caffeineCache;
+    final long startMillis;
+
+    public CaffeineWrapper(int capacity) {
+        assert capacity > 0;
+        this.caffeineCache = Caffeine.newBuilder()
+                .initialCapacity(capacity)
+                .maximumSize(capacity)
+                .recordStats()
+                .expireAfter(new Expiry<K, ValueWrapper<V>>() {
+                    @Override
+                    public long expireAfterCreate(K key, ValueWrapper<V> valueWrapper, long currentTime) {
+                        return Duration.ofMillis(valueWrapper.expireMillis).toNanos();
+                    }
+
+                    @Override
+                    public long expireAfterUpdate(K key, ValueWrapper<V> valueWrapper, long currentTime, long currentDuration) {
+                        return currentDuration;
+                    }
+
+                    @Override
+                    public long expireAfterRead(K key, ValueWrapper<V> valueWrapper, long currentTime, long currentDuration) {
+                        return currentDuration;
+                    }
+                })
+                .build();
+        startMillis = System.currentTimeMillis();
+    }
+
+    @Override
+    public V get(K key, long minExpireMillis, long maxExpireMillis, Function<K, V> loadValueFunction) {
+        assert key != null;
+        assert loadValueFunction != null;
+        assert minExpireMillis >= 0;
+        assert maxExpireMillis >= 0;
+        assert minExpireMillis <= maxExpireMillis;
+        return caffeineCache.get(key, k -> {
+            long expireMillis = ThreadLocalRandom.current().nextLong(minExpireMillis, maxExpireMillis + 1);
+            return new ValueWrapper<>(loadValueFunction.apply(k), expireMillis);
+        }).value;
+    }
+
+    @Override
+    public V getIfPresent(K key) {
+        assert key != null;
+        ValueWrapper<V> valueWrapper = caffeineCache.getIfPresent(key);
+        return valueWrapper == null ? null : valueWrapper.value;
+    }
+
+    @Override
+    public void put(K key, V value, long minExpireMillis, long maxExpireMillis) {
+        assert key != null;
+        assert minExpireMillis >= 0;
+        assert maxExpireMillis >= 0;
+        assert minExpireMillis <= maxExpireMillis;
+        long expireMillis = ThreadLocalRandom.current().nextLong(minExpireMillis, maxExpireMillis + 1);
+        caffeineCache.put(key, new ValueWrapper<>(value, expireMillis));
+    }
+
+    @Override
+    public void remove(K key) {
+        assert key != null;
+        caffeineCache.invalidate(key);
+    }
+
+    @Override
+    public CacheStats stats() {
+        com.github.benmanes.caffeine.cache.stats.CacheStats stats = caffeineCache.stats();
+        return new CacheStats((int) caffeineCache.estimatedSize(), startMillis, System.currentTimeMillis(), stats.requestCount(), stats.hitCount());
+    }
+
+    static class ValueWrapper<V> {
+        final V value;
+        final long expireMillis;
+
+        ValueWrapper(V value, long expireMillis) {
+            this.value = value;
+            this.expireMillis = expireMillis;
+        }
+    }
+}
--- src/test/java/com/github/jiangwangyang/jcache/CachePerformanceTest.java
+++ src/test/java/com/github/jiangwangyang/jcache/CachePerformanceTest.java
@@ -1,19 +1,11 @@
 package com.github.jiangwangyang.jcache;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import com.github.benmanes.caffeine.cache.Expiry;
-import org.checkerframework.checker.index.qual.NonNegative;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.LongAdder;
-import java.util.function.Function;
 
 public class CachePerformanceTest {
 
@@ -25,60 +17,16 @@ public class CachePerformanceTest {
     static final long MAX_EXPIRE_MILLIS = 2000;
 
     @Test
-    void testConcurrentHashMapPerformance() throws InterruptedException {
-        Map<Integer, Long> cache = new ConcurrentHashMap<>(10000);
-        CacheGetFunction<Integer, Long> cacheGetFunction = (key, loadValueFunction, minExpireMillis, maxExpireMillis)
-                -> cache.computeIfAbsent(key, loadValueFunction);
-        testPerformance(cacheGetFunction);
-    }
-
-    @Test
     void testSegmentedLRUCachePerformance() throws InterruptedException {
-        JCache<Integer, Long> cache = new SegmentedLRUCache<>(10000, 100, 0.25);
-        CacheGetFunction<Integer, Long> cacheGetFunction = (key, loadValueFunction, minExpireMillis, maxExpireMillis)
-                -> cache.get(key, minExpireMillis, maxExpireMillis, loadValueFunction);
-        testPerformance(cacheGetFunction);
+        testPerformance(new SegmentedLRUCache<>(10000, 100, 0.25));
     }
 
     @Test
     void testCaffeinePerformance() throws InterruptedException {
-        Cache<Integer, Long> cache = Caffeine.newBuilder()
-                .initialCapacity(10000)
-                .maximumSize(10000)
-                .expireAfter(new Expiry<Integer, Long>() {
-                    @Override
-                    public long expireAfterCreate(Integer k, Long v, long l) {
-                        return Duration.ofMillis(v).toNanos();
-                    }
-
-                    @Override
-                    public long expireAfterUpdate(Integer k, Long v, long l1, @NonNegative long l2) {
-                        return l2;
-                    }
-
-                    @Override
-                    public long expireAfterRead(Integer k, Long v, long l1, @NonNegative long l2) {
-                        return l2;
-                    }
-                })
-                .recordStats()
-                .build();
-        CacheGetFunction<Integer, Long> cacheGetFunction = (key, loadValueFunction, minExpireMillis, maxExpireMillis)
-                -> cache.get(key, loadValueFunction);
-        testPerformance(cacheGetFunction);
+        testPerformance(new CaffeineWrapper<>(10000));
     }
 
-    private void testPerformance(CacheGetFunction<Integer, Long> cacheGetFunction) throws InterruptedException {
-        Function<Integer, Long> loadValueFunction = k -> {
-            if (IO_MILLIS > 0) {
-                try {
-                    Thread.sleep(IO_MILLIS);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            return ThreadLocalRandom.current().nextLong(MIN_EXPIRE_MILLIS, MAX_EXPIRE_MILLIS);
-        };
+    private void testPerformance(JCache<Integer, Integer> cache) throws InterruptedException {
         LongAdder longAdder = new LongAdder();
         ExecutorService threadPool = Executors.newFixedThreadPool(TEST_THREADS);
         for (int i = 0; i < TEST_THREADS; i++) {
@@ -86,7 +34,16 @@ public class CachePerformanceTest {
                 for (; ; ) {
                     Integer key = ThreadLocalRandom.current().nextInt(100) < HOT_PERCENT ?
                             1 : ThreadLocalRandom.current().nextInt();
-                    cacheGetFunction.apply(key, loadValueFunction, MIN_EXPIRE_MILLIS, MAX_EXPIRE_MILLIS);
+                    cache.get(key, MIN_EXPIRE_MILLIS, MAX_EXPIRE_MILLIS, k -> {
+                        if (IO_MILLIS > 0) {
+                            try {
+                                Thread.sleep(IO_MILLIS);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        return k;
+                    });
                     longAdder.increment();
                 }
             });
@@ -99,10 +56,7 @@ public class CachePerformanceTest {
             System.out.println("QPS:" + (double) (cur - prev));
             prev = cur;
         }
-    }
-
-    private interface CacheGetFunction<K, V> {
-        void apply(K key, Function<K, V> loadValueFunction, long minExpireMillis, long maxExpireMillis);
+        threadPool.shutdownNow();
     }
 
 }
